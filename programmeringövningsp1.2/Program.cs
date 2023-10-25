654c38f7f28bd8b391a406c2a097e5811581d26f
DommSite <johan.johansson06@live.se>
2023-10-25 10:40:55 +0200

    Add project files.

---
+++ programmeringövningsp1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tärningskast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            Console.WriteLine("Hur många tärningar vill du kasta?");
+            int antaltärningar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många sidor har tärningarna?");
+            int antalsidor = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i < antaltärningar+1; i++)
+            {
+                int landadepå = slump.Next(1, antalsidor+1);
+                Console.WriteLine("Tärning nummer " + i + " landade på " + landadepå);
+            }
+
+        }
+    }
+}
